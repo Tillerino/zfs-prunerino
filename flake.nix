67f86f6ce68e71224fcc7ec599e24d93e8a3d347
Tillmann Gaida <tillmann.gaida@gmail.com>
2025-12-01 09:19:14 +0100

    flake: upgrade

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
   outputs = {self, nixpkgs, ...}@inputs: let
     forAllSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
   in {
     packages = forAllSys (system: let
       pkgs = import nixpkgs { inherit system; };
-      zfs-prunerino = pkgs.python3Packages.buildPythonPackage {
+      zfs-prunerino = with pkgs.python3Packages; buildPythonPackage {
         name = "zfs-prunerino";
         src = ./.;
         version = "0.1";
@@ -16,6 +16,8 @@
           homepage = "https://github.com/Tillerino/zfs-prunerino";
           license = pkgs.lib.licenses.asl20;
         };
+        format = "setuptools";
+        buildsystem = [ setuptools wheel ];
       };
     in {
       default = zfs-prunerino;
